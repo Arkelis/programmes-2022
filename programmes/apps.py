44b8c302898cda610cfad7709a3e55eefc47e6ce
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2022-01-20 21:11:10 +0100

    Compile SCSS on reload

--- programmes/apps.py
+++ programmes/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from programmes.callbacks import compile_scss
+from django.utils import autoreload
 
 class ProgrammesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "programmes"
+
+
+autoreload.autoreload_started.connect(compile_scss)
+autoreload.file_changed.connect(compile_scss)
